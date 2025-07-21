8e7f08a32b67621ac80329ecfefcb7bbc25c2db9
Raul <raulalfonzo66@gmail.com>
2025-07-21 11:18:28 -0400

    feat (store): create zustand store

---
+++ src/store/store.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+
+interface BusinessInfo {
+  emissionDate: Date | null;                       
+  socialReasonAgent: string | null;    // Nombre o razón social del agente 
+  fiscalAgent: string | null;          // Registro de información fiscal del agente
+  fiscalPeriod: Date | null;           // Período fiscal
+  fiscalAddress: string | null;        // Dirección fiscal del agente
+  socialReasonSubjetc: string | null;  // Nombre o razón social del sujeto
+  subjectFif: string | null;                  // Registro de información fiscal del sujeto
+}
+
+type Store = {
+  businessInfo: BusinessInfo | null
+  setBusinessInfo: (business: BusinessInfo) => void
+}
+
+
+const useStore = create<Store>()(
+  devtools((set) => ({
+    businessInfo: null,
+    setBusinessInfo: (businessInfo: BusinessInfo) => set({ businessInfo }, false, 'setBusinessInfo')
+  }), { name: 'BusinessStore' })
+)
+
+export default useStore;
