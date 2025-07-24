83a864cc580473dfbdcc4d8297412c13a8790ae2
Raul <raulalfonzo66@gmail.com>
2025-07-24 12:07:22 -0400

    Fix (BusinessForm): keep fixing routes here and there

--- src/helpers/routes.ts
+++ src/helpers/routes.ts
@@ -1,6 +1,6 @@
 const base = '/paperwork-pdf-wizard/';
 export const routes = {
-  dashboard: `${base}/`,
-  businessForm: `${base}/business-form`,
-  productForm: `${base}/product-form`,
+  dashboard: `${base}`,
+  businessForm: `${base}business-form`,
+  productForm: `${base}product-form`,
 };
