096fddafdb3188a8ab0d9604372552c657e7eaa8
Raul <raulalfonzo66@gmail.com>
2025-07-24 11:41:24 -0400

    feat (vite.config.ts): Set correct base for deploying at GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/paperwork-pdf-wizard/',
   server: {
     host: "::",
     port: 8080,
